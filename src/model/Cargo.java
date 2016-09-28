bd763a352bfde28c7c5bbcdd63f0042071522215
julio-iuli <carmo.iuli@gmail.com>
2016-09-28 19:25:32 -0300

    get/set Cargo

--- src/model/Cargo.java
+++ src/model/Cargo.java
@@ -7,4 +7,30 @@ public class Cargo {
 
 	public Cargo(){}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+
 }
