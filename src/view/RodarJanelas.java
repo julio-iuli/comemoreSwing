55f8ac35ef4f1decf98970a2f9afac937f8f66ad
julio-iuli <carmo.iuli@gmail.com>
2016-10-22 13:32:49 -0200

    rearranjo do teste

---
+++ src/view/RodarJanelas.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.awt.EventQueue;
+
+public class RodarJanelas {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Teste frame = new Teste();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
--- src/view/Teste.java
+++ src/view/Teste.java
@@ -15,22 +15,6 @@ public class Teste extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Teste frame = new Teste();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public Teste() {
