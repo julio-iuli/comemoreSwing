9c6c2bcf87130842cbb99d12dbb7c692e74c5fa4
julio-iuli <carmo.iuli@gmail.com>
2016-11-07 02:26:21 -0200

    testes com JList

--- src/view/JanelaCargo.java
+++ src/view/JanelaCargo.java
@@ -7,8 +7,10 @@ import java.awt.event.ActionListener;
 
 //teste
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class JanelaCargo extends JPanel implements ActionListener {
+public class JanelaCargo extends JPanel implements ActionListener, ListSelectionListener {
 
 	
 	private JButton criar, editar, excluir;
@@ -18,8 +20,11 @@ public class JanelaCargo extends JPanel implements ActionListener {
 	private String[] cargos = {"Cargo 1", "Cargo 2", 
 			"Cargo 3", "Cargo 4", "Cargo 5", "Cargo 6",
 			"Cargo 7", "Cargo 8", "Cargo 9"};
+	private String cargoEscolhido;
 
 	public JanelaCargo() {
+		
+		cargoEscolhido = new String();
 		
 		setBounds(100, 100, 700, 500);
 		
@@ -46,6 +51,7 @@ public class JanelaCargo extends JPanel implements ActionListener {
 
 		lista = new JList(cargos);
 		lista.setBounds(20, 50, 150, 250);
+		lista.addListSelectionListener(this);
 		
 		campoNome = new JTextField();
 		campoNome.setBounds(20, 325, 200, 20);
@@ -63,7 +69,7 @@ public class JanelaCargo extends JPanel implements ActionListener {
 		add(carSalario);
 		add(carNome);
 		
-		setVisible(true);
+		//setVisible(true);
 		
 	}
 
@@ -71,8 +77,19 @@ public class JanelaCargo extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == criar){
-			JOptionPane.showMessageDialog(null, "funciona! clicado em criar");
+			JOptionPane.showMessageDialog(null, "funciona! clicado em criar: " + cargoEscolhido);
 		}
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent le) {
+		
+		if(le.getSource() == lista){
+			
+			cargoEscolhido = lista.getSelectedValue().toString();
+		}
+		
+
 	}
 
 }
